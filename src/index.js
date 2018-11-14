6b0960eb2a1951b84b8358fc9faec5267b47db56
许振杰 <xuzhenjie@corp.netease.com>
2018-11-14 13:44:35 +0800

    feat: 读取pkg配置

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const chalk = require('chalk');
 const path = require('path');
 const fs = require('fs');
-let gitRoot = require('app-root-path').path;
+const readPkg = require('read-pkg');
+let findUp = require('find-up');
+let appRoot = require('app-root-path').path;
 const msgPath = process.env.HUSKY_GIT_PARAMS;
-//【msgPath】.git/COMMIT_EDITMSG
-console.log('[root]', gitRoot);
-console.log('[msgPath]', msgPath);
+const pkgConifg = readPkg.sync()['gw-commit-lint'];
+const { exclude, types, formats } = pkgConifg;
 
+// 配置检测
+//【msgPath】.git/COMMIT_EDITMSG
+console.log('[root]', appRoot);
+console.log('[msgPath]', msgPath);
+console.log('[pkg.json]', pkgConifg);
 
 module.exports = function gwCommitLint() {
     if(!msgPath) {
@@ -15,16 +21,15 @@ module.exports = function gwCommitLint() {
     }
     
     // find git path
-    while(!fs.existsSync(path.resolve(gitRoot, msgPath))) {
-        gitRoot = path.resolve(gitRoot, '../');
-    }
-    
-    const msg = fs.readFileSync(path.resolve(gitRoot, msgPath), 'utf-8').trim();
+    let gitRoot = findUp.sync('.git');
+    const msg = fs.readFileSync(path.resolve(gitRoot, '../', msgPath), 'utf-8').trim();
+    console.log('[msg]', msg);
     const commitRE = /^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|workflow|ci|chore|types|build|misc)(\(.+\))?: .{0,50}/;
     const mergeRE = /^Merge branch/;
 
+    exclude.forEach()
+
     if (!commitRE.test(msg) && !mergeRE.test(msg)) {
-        console.log()
         console.error(
             `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`invalid commit message format.`)}\n\n` +
             chalk.red(`  Proper commit message format is required for automated changelog generation. Examples:\n\n`) +
@@ -33,7 +38,9 @@ module.exports = function gwCommitLint() {
             chalk.red(`  See .github/COMMIT_CONVENTION.md for more details.\n`) +
             chalk.red(`  About the rules: https://www.conventionalcommits.org/zh/v1.0.0-beta.2/ \n`)
         )
+        console.log(chalk.red('failed'));
         process.exit(1)
     }
+    console.log(chalk.green('pass'));
     process.exit(1) // debug用：无论如何都不通过
 }
